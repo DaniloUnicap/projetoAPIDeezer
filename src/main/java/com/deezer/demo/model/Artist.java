aa475daec23147c9c25cf3bc00c0f603caf43146
DaniloUnicap <danilo.00000850307@unicap.br>
2024-09-05 17:42:38 -0300

    feat: Model entidade Artist

--- src/main/java/com/deezer/demo/model/Artist.java
+++ src/main/java/com/deezer/demo/model/Artist.java
@@ -1,5 +1,27 @@
 package com.deezer.demo.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.NonNull;
+
+@Entity
+@Data
 public class Artist {
-    
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Music> musicList;
 }
