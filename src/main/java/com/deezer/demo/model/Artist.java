37cc96c7cc16833ebbd6bd6310f65e8e914e3093
DaniloUnicap <danilo.00000850307@unicap.br>
2024-09-05 22:26:03 -0300

    feat: Novas funcionalidades Model e Repository de Artist

--- src/main/java/com/deezer/demo/model/Artist.java
+++ src/main/java/com/deezer/demo/model/Artist.java
@@ -2,26 +2,38 @@ package com.deezer.demo.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.Setter;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Artist {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NonNull
     private String name;
 
-    @NonNull
+    
     @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<Music> musicList;
 }
--- src/main/java/com/deezer/demo/respository/ArtistRepository.java
+++ src/main/java/com/deezer/demo/respository/ArtistRepository.java
@@ -1,5 +1,13 @@
 package com.deezer.demo.respository;
 
-public class ArtistRepository {
-    
+import org.springframework.stereotype.Repository;
+import com.deezer.demo.model.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
+
+    public Artist findByName(String name);
 }
+
+
