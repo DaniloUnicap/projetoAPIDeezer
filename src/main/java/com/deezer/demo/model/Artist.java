fc89db4d50ba1e3e5a03117b431a817541be2465
DaniloUnicap <danilo.00000850307@unicap.br>
2024-09-09 18:38:00 -0300

    refactor: Remoção da geração automática de ID na entidade Artist

--- src/main/java/com/deezer/demo/model/Artist.java
+++ src/main/java/com/deezer/demo/model/Artist.java
@@ -7,8 +7,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -26,7 +24,6 @@ import lombok.Setter;
 @Setter
 public class Artist {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NonNull
@@ -35,5 +32,6 @@ public class Artist {
     
     @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnore
+    @NonNull
     private List<Music> musicList;
 }
